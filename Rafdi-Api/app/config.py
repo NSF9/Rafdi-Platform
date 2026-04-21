dfd9a59e76f5e784469d3efadab37fa7559084a3
Nawaf <Alzhraniii999@gmail.com>
2026-04-21 20:41:19 +0300

    new update

--- Rafdi-Api/app/config.py
+++ Rafdi-Api/app/config.py
@@ -1,16 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DB_HOST = os.getenv("DB_HOST", "db")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
-DB_NAME = os.getenv("DB_NAME", "rafdi")
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
-
-print("DB NAME:", DB_NAME)
