c553a7438813c7e7dea86bac028d8194411276a8
Nawaf <Alzhraniii999@gmail.com>
2026-04-29 19:04:41 +0300

    Remove name API and update Docker Compose configuration for database connection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from app.api.name_api import router
 from app.config import engine, Base
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-app.include_router(router)
