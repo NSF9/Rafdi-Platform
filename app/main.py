cc43cd2a664f4714474b67a271f0b77900857b48
NSF9 <alzhraniii999@gmail.com>
2026-05-03 19:10:47 +0300

    feat: add logging configuration to main application

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from app.config import engine
 from app.models.Base_Model import Base,TimestampMixin
 from app.api.Auth_Api import router as auth_router
+import logging
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s - %(message)s"
+)
 from app.models import User, Company, Role, User_Role, Warehouse, Booking, Payment, Notification
 
 app = FastAPI(title="Rafdi Platform")
